8bc92062d62d9b050147c9cbfde9dc2e8fae5444
Abhinav <abhinav.venigalla@gmail.com>
2019-04-20 16:40:25 -0400

    first commit

---
+++ precache-manifest.0bb74f037db7ed77dc1c65c50ce4a6db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0ddbe69ecf367ae88589f95b5fed469",
+    "url": "/static/media/titleinv.a0ddbe69.png"
+  },
+  {
+    "revision": "e34d2bca4a1754170b9aad44c71ffed8",
+    "url": "/static/media/drink.e34d2bca.svg"
+  },
+  {
+    "revision": "62b80d7701c91e129b74189f3f7309a9",
+    "url": "/static/media/clock.62b80d77.svg"
+  },
+  {
+    "revision": "52a3cfafce91a18c938a5f532790567a",
+    "url": "/static/media/arrows.52a3cfaf.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cea983b5e386757bcf24",
+    "url": "/static/js/main.cea983b5.chunk.js"
+  },
+  {
+    "revision": "74761ff27e743ea81b3a",
+    "url": "/static/js/1.74761ff2.chunk.js"
+  },
+  {
+    "revision": "cea983b5e386757bcf24",
+    "url": "/static/css/main.d22db25a.chunk.css"
+  },
+  {
+    "revision": "542d69698467c70efb16ea67e4ad0f2a",
+    "url": "/index.html"
+  }
+];
